a306d2391d4f0f0e1438dd14ee18858e80bf4c9c
ozan honamlioglu <ozanhonamlioglu@hotmail.com>
2020-07-19 00:31:30 +0300

    Initial commit

---
+++ Source/RoomGame/RoomGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoomGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMGAME_API ARoomGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
