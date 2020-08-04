7bb7b78bb2ffd7f2460d18853a9535a6ae377586
ozan honamlioglu <ozanhonamlioglu@hotmail.com>
2020-08-05 02:12:52 +0300

    Default Game Mode is replaced my own blueprint

--- Source/RoomGame/OpenDoor.cpp
+++ Source/RoomGame/OpenDoor.cpp
@@ -9,24 +9,26 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	mCharacter = GetOwner();
 	// ...
 }
-
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	mCharacter = GetOwner();
 	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 void UOpenDoor::OpenDoor()
 {
-	FRotator NewRotation = FRotator(0.f, -90.f, 0.f);
-	mCharacter->SetActorRotation(NewRotation);
+	mCharacter->SetActorRotation(FRotator(0.f, openAngle, 0.f));
 }
 
+void UOpenDoor::CloseDoor()
+{
+	mCharacter->SetActorRotation(FRotator(0.f, 0.f, 0.f));
+}
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -35,6 +37,12 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 
 	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
 		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
 	}
+
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) {
+		CloseDoor();
+	}
+
 }
 
